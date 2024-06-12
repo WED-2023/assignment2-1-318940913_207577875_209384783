88dcf01c722a491e44e9f2bdba7fed0891273e70
DanVaitzman1 <danvai@post.bgu.ac.il>
2024-06-12 09:49:30 +0300

    Removing dist folder

--- dist/js/756.53827942.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkassignment_2_1"]=self["webpackChunkassignment_2_1"]||[]).push([[756],{7756:(s,t,n)=>{n.r(t),n.d(t,{default:()=>r});var e=function(){var s=this;s._self._c;return s._m(0)},a=[function(){var s=this,t=s._self._c;return t("div",{staticClass:"container"},[t("h1",{staticClass:"title"},[s._v("About Page")])])}];const l={},u=l;var c=n(1001),i=(0,c.Z)(u,e,a,!1,null,null,null);const r=i.exports}}]);
-//# sourceMappingURL=756.53827942.js.map
